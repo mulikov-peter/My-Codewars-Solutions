95c2d5a674f4bfa6a3708f3cdc7aef7aa4e80a2a
Peter Mulikov <pettergov1@gmail.com>
2021-02-10 07:40:57 +0100

    8-kyu kata

---
+++ 8-kyu/OppositeNumber.js
@@ -0,0 +1,19 @@
+'use strict';
+/*
+Very simple, given a number, find its opposite.
+Examples:
+1: -1
+14: -14
+-34: 34
+*/
+
+// const opposite = number => {
+//   if (number < 0) return Math.abs(number);
+//   else return -number - 0;
+// };
+
+// const opposite = number => (number < 0 ? Math.abs(number) : -Math.abs(number));
+
+const opposite = number => -number;
+
+// console.log(opposite(1)); // -1;
