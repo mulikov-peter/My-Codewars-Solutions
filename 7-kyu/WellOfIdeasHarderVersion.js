c4f1733fc92d806d2e5fe1f8b0c2514929aa44ef
Peter Mulikov <pettergov1@gmail.com>
2021-02-22 07:58:59 +0100

    7-kyu

---
+++ 7-kyu/WellOfIdeasHarderVersion.js
@@ -0,0 +1,39 @@
+'use strict';
+/* For every good kata idea there seem to be quite a few bad ones!
+
+In this kata you need to check the provided 2 dimensional array (x) for good ideas 'good' and bad ideas 'bad'. If there are one or two good ideas, return 'Publish!', if there are more than 2 return 'I smell a series!'. If there are no good ideas, as is often the case, return 'Fail!'.
+
+The sub arrays may not be the same length.
+
+The solution should be case insensitive (ie good, GOOD and gOOd all count as a good idea). All inputs may not be strings. */
+
+function well(x) {
+  let count = 0;
+  x.forEach(el => {
+    el.forEach(item => {
+      if (typeof item === 'string' && item.toLowerCase() === 'good') count++;
+    });
+  });
+  console.log(count);
+  return count < 1 ? 'Fail!' : count >= 3 ? 'I smell a series!' : 'Publish!';
+}
+
+// Test
+
+// console.log(
+//   well([
+//     ['bad', 'bAd', 'bad'],
+//     ['bad', 'bAd', 'bad'],
+//     ['bad', 'bAd', 'bad'],
+//   ])
+// ); // 'Fail!');
+// console.log(
+//   well([
+//     ['gOOd', 'bad', 'BAD', 'bad', 'bad'],
+//     ['bad', 'bAd', 'bad'],
+//     ['GOOD', 'bad', 'bad', 'bAd'],
+//   ])
+// ); // 'Publish!');
+// console.log(
+//   well([['gOOd', 'bAd', 'BAD', 'bad', 'bad', 'GOOD'], ['bad'], ['gOOd', 'BAD']])
+// ); // 'I smell a series!')
