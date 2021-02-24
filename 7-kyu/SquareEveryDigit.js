02cc395827b3f15259f3d34200850c622440e6d3
Peter Mulikov <pettergov1@gmail.com>
2021-02-24 22:12:56 +0100

    7kyu

---
+++ 7-kyu/SquareEveryDigit.js
@@ -0,0 +1,31 @@
+'use strict';
+/* Welcome. In this kata, you are asked to square every digit of a number and concatenate them.
+
+For example, if we run 9119 through the function, 811181 will come out, because 92 is 81 and 12 is 1.
+
+Note: The function accepts an integer and returns an integer
+*/
+
+// function squareDigits(num) {
+//   let res = [];
+
+//   String(num)
+//     .split('')
+//     .map(n => {
+//       res.push(n * n);
+//     });
+//   return +res.join('');
+// }
+
+const squareDigits = num =>
+  Number(
+    ('' + num)
+      .split('')
+      .map(function (val) {
+        return val * val;
+      })
+      .join('')
+  );
+
+// Test
+// console.log(squareDigits(9119)); // 811181);
