edc31876369480509f3b24cb8429668bf49d8b7b
Peter Mulikov <pettergov1@gmail.com>
2021-02-26 21:35:02 +0100

    6-kyu

---
+++ 6-kyu/ArrayDiff.js
@@ -0,0 +1,20 @@
+'use strict';
+/*Your goal in this kata is to implement a difference function, which subtracts one list from another and returns the result.
+
+It should remove all values from list a, which are present in list b.
+
+arrayDiff([1,2],[1]) == [2]
+If a value is present in b, all of its occurrences must be removed from the other:
+
+arrayDiff([1,2,2,2,3],[2]) == [1,3] */
+
+function arrayDiff(a, b) {
+  return a.filter(e => !b.includes(e));
+}
+//Test
+// console.log(arrayDiff([], [4, 5])); // [], "a was [], b was [4,5]");
+// console.log(arrayDiff([3, 4], [3])); // [4], "a was [3,4], b was [3]");
+// console.log(arrayDiff([1, 8, 2], [])); // [1,8,2], "a was [1,8,2], b was []");
+// console.log(arrayDiff([1, 2, 2, 2, 3], [2])); // == [1,3]
+// console.log(arrayDiff([14, 0], [0])); // == [14]
+// console.log(arrayDiff([9, 16, 10], [9, 16])); // == [10]
