575eb6fc823631059122a0ba8b3aab3189b56537
Peter Mulikov <pettergov1@gmail.com>
2021-02-27 21:15:12 +0100

    7-kyu

---
+++ 7-kyu/ExtendingJavaScriptObjectsGetFirstLastArrayElement.js
@@ -0,0 +1,22 @@
+'use strict';
+/*Your task is to extend JavaScript Array object, with methods .first() and .last(), so you can get respectively first or last element of the array.
+
+var a = [2, 5, 7, 3 ,4];
+
+a.first();  // 2
+a.last();   // 4
+Note: in case of empty array, methods should return undefined. */
+
+// Array.prototype.first = function () {
+//   return this[0];
+// };
+
+// Array.prototype.last = function () {
+//   return this[this.length - 1];
+// };
+
+// Test
+// console.log([2, 5, 7, 3, 4].first()); // 2);
+// console.log([2, 5, 7, 3, 4].last()); //4);
+// console.log([].first()); //undefined);
+// console.log([].last()); //undefined);
