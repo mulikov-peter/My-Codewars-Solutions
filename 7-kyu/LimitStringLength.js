b9c807861ae6b46e0639658c14ecd5e68c2f0de9
Peter Mulikov <pettergov1@gmail.com>
2021-03-01 12:39:03 +0100

    7-kyu

---
+++ 7-kyu/LimitStringLength.js
@@ -0,0 +1,19 @@
+'use strict';
+/* The function must return the truncated version of the given string up to the given limit followed by "..." if the result is shorter than the original. Return the same string if nothing was truncated.
+
+Example:
+
+solution('Testing String', 3) --> 'Tes...'
+solution('Testing String', 8) --> 'Testing ...'
+solution('Test', 8)           --> 'Test'
+*/
+
+// function solution1(string, limit) {
+//    return limit > string.length
+//      ? `${string.slice(0, limit)}`
+//     : `${string.slice(0, limit)}...`;
+// }
+
+// console.log(solution1('Testing String', 3));
+// console.log(solution1('Test', 8));
+// console.log(solution1('Test', 4));
