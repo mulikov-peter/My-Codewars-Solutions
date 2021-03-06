406db017e41f0e69273012a6b434863d0e5356f3
Peter Mulikov <pettergov1@gmail.com>
2021-03-06 11:25:29 +0100

    fc

---
+++ 7-kyu/ownbind.js
@@ -0,0 +1,15 @@
+const logPerson = person => {
+  return `Person: ${person.name}, ${person.age}, ${person.job}`;
+};
+
+const person1 = { name: 'Adam', age: 23, job: 'dev' };
+const person2 = { name: 'Eva', age: 21, job: 'economist' };
+
+const bind = function (person, logPerson) {
+  return logPerson(person);
+};
+
+console.log(bind(person1, logPerson));
+console.log(bind(person2, logPerson));
+
+// bind(person1, logPerson)
