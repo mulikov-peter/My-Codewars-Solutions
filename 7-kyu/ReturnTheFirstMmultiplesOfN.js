f9f9594c2eda743264ed3361e0dea7a766ee8fd1
Peter Mulikov <pettergov1@gmail.com>
2021-03-08 20:41:15 +0100

    7-kyu

---
+++ 7-kyu/ReturnTheFirstMmultiplesOfN.js
@@ -0,0 +1,15 @@
+'use strict';
+/* Implement a function, multiples(m, n), which returns an array of the first m multiples of the real number n. Assume that m is a positive integer.
+
+Ex.
+
+multiples(3, 5.0)
+should return
+
+[5.0, 10.0, 15.0] */
+// const multiples = (m, n) => Array(m).fill(n).map((_, i) => (i + 1) * n);
+
+// Codewar
+// const multiples = (m, n) => Array.from(Array(m), (_, i) => n * i + n);
+
+// console.log(multiples(3, 5)); // [5, 10, 15])
