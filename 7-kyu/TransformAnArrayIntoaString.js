2337ae897db30b53067689d1fd3bf79a376914ae
Peter Mulikov <pettergov1@gmail.com>
2021-03-10 10:56:08 +0100

    7kyu

---
+++ 7-kyu/TransformAnArrayIntoaString.js
@@ -0,0 +1,23 @@
+'use strict';
+/*
+A simple kata, my first.
+simply tranform an array into a string, like so:
+
+  transform([4, -56, true, "box"]) => "4-56truebox"
+have fun coding!
+*/
+
+// const transform =(array) => {
+//   let res = '';
+//   array.forEach(el => (res += el));
+//   return res;
+// }
+
+// const transform = array => array.reduce((acc, cur) => acc + cur, '');
+
+// // Codewar
+// // function transform(array) {
+//   // return array.map(String).join('');
+// // }
+// //Test
+// console.log(transform(['BmX', false, 784])); // "BmXfalse784")
