f1ed90b927302b6c091f56beb621b3f80720ca9b
Peter Mulikov <pettergov1@gmail.com>
2021-03-11 13:55:51 +0100

    7kyu

---
+++ 7-kyu/SumOfCubes.js
@@ -0,0 +1,24 @@
+'use strict';
+/*
+Write a function that takes a positive integer n, sums all the cubed values from 1 to n, and returns that sum.
+
+Assume that the input n will always be a positive integer.
+
+Examples:
+
+sumCubes(2) // 9
+// sum of the cubes of 1 and 2 is 1 + 8 
+*/
+
+// const sumCubes = n =>
+//   Array(n)
+//     .fill(1)
+//     .reduce(
+//       (acc, cur, i) => acc + cur * (i + 1) * cur * (i + 1) * cur * (i + 1),
+//       0
+//     );
+
+// console.log(sumCubes(1)); // 1
+// console.log(sumCubes(2)); // 9
+// console.log(sumCubes(3)); // 36
+// console.log(sumCubes(4)); // 100
