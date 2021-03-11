0a10825f16d9776ae390525b1159ae06c1c4631c
Peter Mulikov <pettergov1@gmail.com>
2021-03-11 14:15:51 +0100

    7kyu

---
+++ 7-kyu/NoOdditiesHere.js
@@ -0,0 +1,11 @@
+'use strict';
+/*
+Write a small function that returns the values of an array that are not odd.
+
+All values in the array will be integers. Return the good values in the order they are given.
+
+*/
+// const noOdds = values => values.filter(n => n % 2 === 0);
+
+// // Test
+// console.log(noOdds([0, 1, 2, 3])); //, [0,2]
