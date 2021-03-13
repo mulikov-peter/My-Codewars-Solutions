44b6c8b38e24559953e4f688fa1353ad6be1090d
Peter Mulikov <pettergov1@gmail.com>
2021-03-13 16:13:24 +0100

    7 kyu

---
+++ 7-kyu/RotateToTheMax.js
@@ -0,0 +1,26 @@
+'use strict';
+
+/* Task
+Given a number, return the maximum value by rearranging its digits.
+
+Examples:
+
+(123) => 321 (786) => 876 ("001") => 100 (999) => 999 (10543) => 54310
+
+Note the number may be given as a string
+*/
+
+// const rotateToMax = n =>
+//   Number(
+//     String(n)
+//       .split('')
+//       .sort((a, b) => b - a)
+//       .join('')
+//   );
+
+// Test
+// console.log(rotateToMax(123)); // 321)
+// console.log(rotateToMax(786)); // 876);
+// console.log(rotateToMax('001')); // 100)
+// console.log(rotateToMax(999)); // 999)
+// console.log(rotateToMax(10543)); // 54310)
